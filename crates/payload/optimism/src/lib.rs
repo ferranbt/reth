551d734ba09733edd12b53a2e142950116bb1ca8
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-06-14 11:22:05 -0400

    More stuff

--- crates/payload/optimism/src/lib.rs
+++ crates/payload/optimism/src/lib.rs
@@ -12,7 +12,7 @@
 #![cfg(feature = "optimism")]
 
 pub mod builder;
-pub use builder::OptimismPayloadBuilder;
+pub use builder::{optimism_payload_builder, OptimismPayloadBuilder};
 pub mod error;
 pub mod payload;
 pub use payload::{OptimismBuiltPayload, OptimismPayloadBuilderAttributes};
